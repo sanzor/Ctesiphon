d669f038a1264e485d31331ad7a91cbb6b18d25f
Adrian Bercovici <adriansan_93@yahoo.com>
2020-04-14 22:34:33 +0300

    implement

--- Core/Server/UnityChatApi.Server.Core/Services/ChatClient.cs
+++ Core/Server/UnityChatApi.Server.Core/Services/ChatClient.cs
@@ -8,6 +8,9 @@ using UnityChatApi.DataAccess;
 using UnityChatApi.Extensions;
 using UnityChatApi.Models;
 using StackExchange.Redis;
+using System.Collections.Concurrent;
+using System.Buffers;
+using System.Threading;
 
 namespace UnityChatApi.Server.Core {
     public sealed class ChatClient {
@@ -17,11 +20,13 @@ namespace UnityChatApi.Server.Core {
         private WebSocket socket;
         private RedisStore store;
         private ISubscriber sub;
+        private BlockingCollection<RedisValue> queue = new BlockingCollection<RedisValue>();
+        
         public ChatClient(WebSocket socket, RedisStore store) {
             this.socket = socket;
             this.store = store;
-
         }
+
         public async Task RunAsync() {
             try {
                 this.sub = this.store.Connection.GetSubscriber();
@@ -29,17 +34,25 @@ namespace UnityChatApi.Server.Core {
                 this.popperTask = Task.Run(async () => await PopLoopAsync());
                 await Task.WhenAny(writeTask,popperTask);
             } catch (Exception ex) {
-
                 throw;
             }
 
         }
 
-        private async Task WriteLoopAsync() {
+        private async Task HandleMessageAsync(Message msg) {
 
+            //subscribe to channel if it is not already subscribed;
+            await this.sub.SubscribeAsync(msg.Channel, (channel, value) => {
+                this.queue.Add(value);
+             });
+            await this.sub.PublishAsync(msg.Channel, msg.Value);
+        }
+
+        private async Task WriteLoopAsync() {
+            var buffer=ArrayPool<byte>.Shared.Rent(1024);
             while (true) {
                 //receive some message from socket
-                var message = await this.socket.ReceiveAsync([....]); 
+                var result = await this.socket.ReceiveAsync(buffer, CancellationToken.None);
                 //find list of subscribed channels and if it does not exist subscribe to it
                 //publish message to target channel
                 await HandleMessageAsync(message);
@@ -50,17 +63,13 @@ namespace UnityChatApi.Server.Core {
             
             while (true) {
                 //pop a message from the queue that is filled by channel delegates
-                var data = await this.store.Connection.BLPOP("popList");
+                var data =this.queue.Take();
                 //send the message on the websocket
-                this.socket.SendAsync(data);
+                await this.socket.SendAsync(data.decode);
             }
         }
-        private async Task HandleMessageAsync(Message msg) {
-            //subscribe to channel if it is not already subscribed;
-            await this.sub.SubscribeAsync(msg.Channel, async (channel, value) => {
-                await this.store.Database.ListRightPushAsync("popList", msg.Value);
-            });
-            await this.sub.PublishAsync(msg.Channel, msg.Value);
-        }
+       
+     
+       
     }
 }
