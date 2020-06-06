1bfeee6667650272f49bd7a5ec6039ddeff83a17
Adrian Bercovici <adriansan_93@yahoo.com>
2020-06-06 23:18:13 +0300

    doing the dataflow

--- Tests/Client/PubSubClient.cs
+++ Tests/Client/PubSubClient.cs
@@ -5,6 +5,11 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using PubSubSharp.Extensions;
+using PubSubSharp.Models;
+using System.Threading.Tasks.Dataflow;
+using System.Buffers;
+using System.Text.Json;
 
 namespace Client {
     public class PubSubClient {
@@ -13,6 +18,8 @@ namespace Client {
         private ILogger logger = Log.ForContext<PubSubClient>();
         private Task loopTask;
 
+
+
         public static Task RunAsync(ClientWebSocket clientSocket) {
             PubSubClient client = new PubSubClient(clientSocket);
             return client.InnerRunAsync();
@@ -20,16 +27,35 @@ namespace Client {
         private Task InnerRunAsync() {
             this.loopCts = new CancellationTokenSource();
             logger.Information("Starting long running task");
-            this.loopTask = Task.Run(async() => await LoopAsync(this.loopCts.Token),this.loopCts.Token);
+            this.loopTask = Task.Run(async () => await LoopAsync(this.loopCts.Token), this.loopCts.Token);
         }
 
 
         private async Task LoopAsync(CancellationToken token) {
-            
+            var bufferBlock = new BufferBlock<byte[]>(new DataflowBlockOptions { BoundedCapacity = 10 });
+            var toMessageBlock = new TransformBlock<byte[], ChatMessage>(x => {
+                try {
+                    var message=JsonSerializer.Deserialize<ChatMessage>(Encoding.UTF8.GetString(x));
+                    return message;
+                } catch (Exception ex) {
+
+                    throw;
+                }
+            }
+            , new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = ExecutionDataflowBlockOptions.Unbounded });
+            var actionBlock = new ActionBlock<ChatMessage>(x => Console.WriteLine($"I received a message at channel:{x.Channel} of type:{x.Kind}"));
+
+            bufferBlock.LinkTo(toMessageBlock);
+            toMessageBlock.LinkTo(actionBlock);
+            var memory = ArrayPool<byte>.Shared.Rent(1024);
             while (true) {
                 if (token.IsCancellationRequested) {
                     break;
                 }
+                var message = await wsocket.ReceiveAsync(memory, CancellationToken.None);
+                var data=memory.AsSpan().Slice(0, message.Count).ToArray();
+                bufferBlock.Post(data);
+               
             }
         }
         public PubSubClient(ClientWebSocket connectedClientSocket) {
@@ -37,4 +63,7 @@ namespace Client {
         }
 
     }
+
+
 }
+
