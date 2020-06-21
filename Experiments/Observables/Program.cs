29cd389acdef51647bc68bc0efad2f8d4e5ac6f8
Adrian Bercovici <adriansan_93@yahoo.com>
2020-06-21 21:22:43 +0300

    infintie sequence

---
+++ Experiments/Observables/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Observables {
+    class Program {
+        public static  async Task<string> ProduceAsync() {
+
+            await Task.Delay(1);
+            var k = Console.ReadKey();
+            if (k.Key == ConsoleKey.A) {
+                throw new Exception("Invalid key");
+            }
+
+            return k.Key.ToString();
+            
+        }
+        public static IEnumerable<IObservable<string>> GetEndless() {
+            for (; ; ) {
+                var produceObs = Observable.FromAsync(ProduceAsync);
+                var loop = produceObs.Repeat();
+                yield return loop;
+            }
+        }
+        private static EventWaitHandle handle = new EventWaitHandle(false,EventResetMode.AutoReset);
+        
+        static void Main(string[] args) {
+            var endless=Observable.Catch(GetEndless());
+            var sub = endless.SubscribeOn(NewThreadScheduler.Default).Subscribe(
+                onNext: y => Console.WriteLine($"\n{DateTime.Now.ToShortDateString()}\t{y}"),
+                onCompleted:()=>Console.WriteLine("done"),
+                onError:ex=>Console.WriteLine($"{ex.Message}"));
+          
+          
+            
+            handle.WaitOne();
+           
+        }
+    }
+}
--- Tests/Client/Program.cs
+++ Tests/Client/Program.cs
@@ -5,8 +5,11 @@ using Serilog;
 
 using System;
 using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net.WebSockets;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Reflection;
 using System.Text;
@@ -17,6 +20,7 @@ namespace Client {
     class Program {
         private const string URL = "ws://localhost:8600";
         private const string TEST_CHANNEL = "test";
+        private static EventWaitHandle handle = new EventWaitHandle(false, EventResetMode.AutoReset);
         public static string ToCurrentAssemblyRootPath(string target) {
             var path = Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly().FullName).FullName, target);
             return path;
@@ -30,32 +34,41 @@ namespace Client {
            .CreateLogger();
         }
         static async Task Main(string[] args) {
-
+            IEnumerable<IObservable<ChatMessage>> GetFactory(ClientWebSocket socket){
+                for (; ; ) {
+                    var obb = Observable.FromAsync(async (cts) => {
+                        Memory<byte> data = ArrayPool<byte>.Shared.Rent(1024);
+                        return await socket.ReceiveAndDecodeAsync<ChatMessage>(CancellationToken.None);
+                    });
+                    var loop = obb.Repeat();
+                    yield return loop;
+                }
+            }
             ClientWebSocket clientsocket = new ClientWebSocket();
             CancellationTokenSource loopCTS = new CancellationTokenSource();
             await clientsocket.ConnectAsync(new Uri(URL), CancellationToken.None);
             await clientsocket.SendAsync(new ChatMessage { SenderID = "Adisor", Kind = ChatMessage.DISCRIMINATOR.SUBSCRIBE, Channel = TEST_CHANNEL }.Encode(), WebSocketMessageType.Text, true, CancellationToken.None);
             PubSubClient client = new PubSubClient(clientsocket);
 
-            var obb = Observable.FromAsync(async (cts) => {
-                Memory<byte> data = ArrayPool<byte>.Shared.Rent(1024);
-                return await clientsocket.ReceiveAndDecodeAsync<ChatMessage>(loopCTS.Token);
-            }).Catch(Observable.Empty<ChatMessage>()).Repeat();
 
-            obb.Subscribe(x => Console.WriteLine(x.ToJson()), x => Console.WriteLine("Done"), CancellationToken.None);
+            var endless = GetFactory(clientsocket).Catch();
 
-            try {
-                while (!(Console.ReadKey().Key == ConsoleKey.Q)) ;
-                loopCTS.Cancel();
-            } catch (OperationCanceledException ex) {
-                Log.Information("Cancel was issued");
-            } catch (Exception ex) {
-                Log.Error($"Closing due to error.\tReason:{ex.Message}");
-            }
+            endless.Subscribe(
+                onNext: x => {
+                    var t = 2 + 1;
+                    Process.Start(new ProcessStartInfo { FileName = "werl.exe" ,Arguments=$"-sname {new Random().Next(0,100)}_{x.Channel} -setcookie {x.Channel}"});
+                    
+                },
+                onCompleted: () => Console.WriteLine("done"),
+                onError: z => Console.WriteLine($"{z.Message}"),
+                token:loopCTS.Token
+                );
 
+            handle.WaitOne();
 
 
         }
 
     }
 }
+
