b30b51522dd1b5e167382ea7c54ec22fce03571b
Adrian Bercovici <adriansan_93@yahoo.com>
2020-12-26 00:52:14 +0200

    present

--- Core/Server/PubSubSharp.Server/Constants.cs
+++ Core/Server/PubSubSharp.Server/Constants.cs
@@ -5,6 +5,6 @@ namespace PubSubSharp.Server {
         public const string LOG_FILE = @"log/log.txt";
         public const string CONFIG_FILE = "appsettings.json";
         public const string LOG_OUTPUT_TEMPLATE = "{Timestamp: HH:mm: ss} [{Level:u3}] {Properties} {Message:lj}{NewLine}{Exception}";
-        public const string CORELLATION_ID = "corellationId";
+        
     }
 }
--- Core/Server/PubSubSharp.Server/Program.cs
+++ Core/Server/PubSubSharp.Server/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using PubSubSharp.Conventions;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
